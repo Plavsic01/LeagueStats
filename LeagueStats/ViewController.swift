7eb751a8ce18d834946b71a9d91fe2d5f43d5d0e
Andrej <andrejplavsic3@gmail.com>
2022-09-23 13:50:43 +0200

    Initial Commit

---
+++ LeagueStats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeagueStats
+//
+//  Created by Andrej Plavsic on 23.9.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
